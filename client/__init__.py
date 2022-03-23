ac02cd2fb537d0d9506150b0d14f50d0db02ce0f
sensiherme <102251140+sensiherme@users.noreply.github.com>
2022-03-24 01:32:36 +0430

    Add files via upload

---
+++ client/__init__.py
@@ -0,0 +1,10 @@
+# Sensi - UserBot
+# Copyright (c) 2022 Sensi-Userbot
+# Credits: @indraudah || https://github.com/annubishermes/hermesubot
+#
+# This file is a part of < https://github.com/annubishermes/hermesubot >
+# 
+
+from .client_list import client_id, clients_list
+from .logger import man_userbot_on
+from .startup import man_client, multiman
---
+++ client/logger.py
@@ -0,0 +1,74 @@
+# Sensi - UserBot
+# Copyright (c) 2022 Sensi-Userbot
+# Credits: @indraudah || https://github.com/annubishermes/
+#
+# This file is a part of <https://github.com/annubishermes/hermesubot>
+# 
+
+from telethon.tl.functions.channels import InviteToChannelRequest
+
+from userbot import BOT_USERNAME
+from userbot import BOT_VER as version
+from userbot import BOTLOG_CHATID
+from userbot import CMD_HANDLER as cmd
+from userbot import MAN2, MAN3, MAN4, MAN5, bot, branch
+
+MSG_ON = """
+ ⚡**Sensi-Userbot Berhasil Di Deploy**⚡
+━━
+➥ **Userbot Version -** `{}@{}`
+➥ **Ketik** `{}ping` **untuk Mengecheck Bot**
+━━
+"""
+
+
+async def man_userbot_on():
+    try:
+        if bot:
+            if BOTLOG_CHATID != 0:
+                await bot.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
+    try:
+        if MAN2:
+            if BOTLOG_CHATID != 0:
+                await MAN2.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
+    try:
+        if MAN3:
+            if BOTLOG_CHATID != 0:
+                await MAN3.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
+    try:
+        if MAN4:
+            if BOTLOG_CHATID != 0:
+                await MAN4.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
+    try:
+        if MAN5:
+            if BOTLOG_CHATID != 0:
+                await MAN5.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
+    try:
+        await bot(InviteToChannelRequest(int(BOTLOG_CHATID), [BOT_USERNAME]))
+    except BaseException:
+        pass
